5763aa7e97dc02b10da6226cabfa615ded1711de
Veeru <immaculateempyrean@gmail.com>
2021-08-09 09:54:41 -0700

    contact page is shaping up nicely.

---
+++ src/components/ContactPanel.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="contact-panel">
+        <MainTextDisplay :heading1="heading1" :heading2="heading2" :description="description"
+                         :showButton="showButton" :showCaption="showCaption"/>
+        <ul>
+            <li>Send me an Email at <strong>immaculateEmpyrean@gmail.com</strong></li>
+            <li>I am on Discord: <img src= "@/assets/discord-profile-picture.png"> <strong>empyrean#4242</strong></li>
+            <li><a href="">Have a Github preference?</a></li>
+            <li>My LinkedIn page</li>
+            <li>Leave a message here on this site</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import MainTextDisplay from './MainTextDisplay.vue';
+
+export default {
+    name: "ContactPanel",
+    components: {
+        MainTextDisplay
+    },
+    data (){
+        return {
+            heading1: "Let",
+            heading2: "Us Talk",
+            showCaption: false,
+            showButton: false,
+            description: ""
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+    .contact-panel{
+        flex: 1 1 45%;
+
+        display: flex;
+        flex-direction: column;
+    }
+
+    li{
+        img{
+            border-radius: 50%;
+        }
+    }
+</style>
--- src/views/ContactMe.vue
+++ src/views/ContactMe.vue
@@ -1,10 +1,31 @@
 <template>
-    ContactMe Page
+    <div class="contact-wrapper">
+        <ContactPanel />
+        <MainImageDisplay :imagePath="imagePath" />
+    </div>
 </template>
 
 <script>
+import MainImageDisplay from '../components/MainImageDisplay.vue';
+import ContactPanel from '../components/ContactPanel.vue';
 
 export default {
-    name: 'ContactMe'
+    name: 'ContactMe',
+    components:{
+        ContactPanel,
+        MainImageDisplay
+    },
+    data(){
+        return {
+            imagePath: require('@/assets/contact-me.png')
+        }
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+    .contact-wrapper{
+        flex: 1 1 93%;
+        display: flex;
+    }
+</style>
