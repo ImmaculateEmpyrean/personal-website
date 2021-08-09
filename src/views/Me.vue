ceed0b6aa6db9b5b625fbdaf99552eee1c38a10f
Veeru <immaculateempyrean@gmail.com>
2021-08-09 09:08:08 -0700

    I fixed the me page.

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -1,5 +1,6 @@
 <template>
-    <Content />
+    <Content :heading1="heading1" :heading2="heading2" :caption="caption" :description="description"
+             :showCaption="showCaption" :showButton="showButton" :imagePath="imagePath"/>
 </template>
 
 <script>
@@ -9,6 +10,18 @@ export default {
     name: 'Me',
     components: {
         Content
+    },
+    data (){
+        return {
+            heading1:"Me",
+            heading2:"Automatons",
+            caption: "NULL",
+            showCaption: false,
+            description: "I was definitely the cool kid (PROBABLY), reading russian and german literature \
+                                        definitely gave me enough imagination to believe something like that.",
+            showButton: true,
+            imagePath: require("@/assets/me.png")
+        }
     }
 }
 </script>
