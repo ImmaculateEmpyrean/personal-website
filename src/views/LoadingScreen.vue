b14d28ad13ec44b27d5ac24677ea43754fa5938e
Veeru <immaculateempyrean@gmail.com>
2021-08-10 05:27:41 -0700

    LoadingScreen written.

--- src/views/LoadingScreen.vue
+++ src/views/LoadingScreen.vue
@@ -1,5 +1,18 @@
 <template>
-    LoadingScreen
+    <div class="loading-component">
+        <div class="loader">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <h1>Sending Mail</h1>
+    </div>
 </template>
 
 <script>
@@ -7,3 +20,64 @@ export default {
     name: "LoadingScreen"
 }
 </script>
+
+<style lang="scss" scoped>
+
+    .loading-component{
+        flex: 1 1 100%;
+        align-self: center;
+        
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .loader {
+        position: relative;
+        display: grid;
+        grid-template-columns: 33% 33% 33%;
+        grid-gap: 2px;
+        width: 100px;
+        height: 100px;
+    }
+    .loader > div {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        background: red;
+        transform: scale(0);
+        transform-origin: center center;
+        animation: loader 2s infinite linear;
+    }
+    .loader > div:nth-of-type(1), .loader > div:nth-of-type(5), .loader > div:nth-of-type(9) {
+        animation-delay: 0.4s;
+    }
+    .loader > div:nth-of-type(4), .loader > div:nth-of-type(8) {
+        animation-delay: 0.2s;
+    }
+    .loader > div:nth-of-type(2), .loader > div:nth-of-type(6) {
+        animation-delay: 0.6s;
+    }
+    .loader > div:nth-of-type(3) {
+        animation-delay: 0.8s;
+    }
+    @keyframes loader {
+        0% {
+            transform: scale(0);
+        }
+        40% {
+            transform: scale(1);
+        }
+        80% {
+            transform: scale(1);
+        }
+        100% {
+            transform: scale(0);
+        }
+    }
+
+    h1{
+        font-family: 'Futura Bk';
+        margin-left: 50px;
+    }
+</style>
