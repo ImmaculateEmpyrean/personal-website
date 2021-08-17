69525eb3180973c13d7c0905185121238ea71208
Veeru <immaculateempyrean@gmail.com>
2021-08-17 08:28:19 -0700

    added automation to deploy to heroku directly.

--- back-end/build-distribution.js
+++ back-end/build-distribution.js
@@ -4,5 +4,12 @@ const path = require('path');
 fs.emptyDirSync(path.resolve(__dirname,"dist"));
 fs.copySync(path.resolve(__dirname,"src"),path.resolve(__dirname,"dist"));
 
-fs.copySync(path.resolve(__dirname,"package.json"),path.resolve(__dirname,"dist/package.json"));
+// fs.copySync(path.resolve(__dirname,"package.json"),path.resolve(__dirname,"dist/package.json"));
+//remove the script build from package.json
+let file = fs.readFileSync(path.resolve(__dirname,"package.json"), 'utf8');
+obj = JSON.parse(file); //now it an object
+obj.scripts.build="";
+let json = JSON.stringify(obj); //convert it back to json
+fs.writeFileSync(path.resolve(__dirname,"dist/package.json"), json, 'utf8'); // write it back 
+
 fs.copySync(path.resolve(__dirname,"package-lock.json"),path.resolve(__dirname,"dist/package-lock.json"));
---
+++ build-deploy-distribution-heroku.js
@@ -0,0 +1,39 @@
+const fs = require('fs-extra');
+const path = require('path');
+const {execSync} = require('child_process');
+
+let deploymentTracker = require('./heroku-deployment-tracker.json');
+const { stringify } = require('querystring');
+
+require('./build-distribution.js');
+
+fs.copySync(path.resolve(__dirname,"./back-end/Procfile"),path.resolve(__dirname,"dist/Procfile"));
+
+execSync('git init',{
+    cwd: path.join(__dirname,"dist"),
+    stdio: 'inherit'
+});
+
+execSync('heroku git:remote -a veeru-empyrean',{
+    cwd: path.join(__dirname,"dist"),
+    stdio: 'inherit'
+});
+
+execSync('git add -A',{
+    cwd: path.join(__dirname,"dist"),
+    stdio: 'inherit'
+});
+
+let latestIndex = deploymentTracker.nextVersion;
+deploymentTracker.nextVersion++;
+fs.writeFileSync('./heroku-deployment-tracker.json',JSON.stringify(deploymentTracker), 'utf8');
+
+execSync(`git commit -m "Version : ${latestIndex} being deployed"`,{
+    cwd: path.join(__dirname,"dist"),
+    stdio: 'inherit'
+});
+
+execSync(`git push heroku master`,{
+    cwd: path.join(__dirname,"dist"),
+    stdio: 'inherit'
+});
